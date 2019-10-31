f5e7288c44b3fd80ea9a7b44d37b64528b13de28
Harshpinder singh <dhaulaharsh@gmail.com>
2019-11-01 00:02:34 +0530

    bug fix

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ app.use(express.json())
 app.use(cors())
 app.use('/api', router)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "client/build")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname + "/client/build/index.html"))
-    })
-}
+
+app.use(express.static(path.join(__dirname, "client/build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"))
+})
 
 app.listen(process.env.PORT || 3005, () => {
     console.log('listening on  3005')
